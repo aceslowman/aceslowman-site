f3c7425a75e336bb9028dd31302eea3af54e2cba
aceslowman <aceslowman@gmail.com>
2017-10-23 14:01:36 -0600

    Reorganized and began using react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
-import Navigation from './Navigation';
-import Home from './Home/Home';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Notebook from './components/Notebook';
 import './App.css';
+
+import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
         <div>
-            <header>
-                <h1>aceslowman</h1>
-            </header>
-            <Navigation />
-            <Home />
+            <div className="top">
+                <header>
+                    <Link to="/"><h1>aceslowman</h1></Link>
+                </header>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/projects">Projects</Link>
+                        </li>
+                        <li>
+                            <Link to="/notebook">Notebook</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="content">
+                <Route path="/" component={Home} exact="true"/>
+                <Route path="/about" component={About}/>
+                <Route path="/projects" component={Projects}/>
+                <Route path="/notebook" component={Notebook}/>
+            </div>
         </div>
     )
   }
