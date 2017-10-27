0e55581efa51519ae79ad64859b3a8f037a67a8f
aceslowman <aceslowman@gmail.com>
2017-10-26 20:41:53 -0600

    Trying to prevent the Google Chrome pull-to-refresh

--- public/js/script.js
+++ public/js/script.js
@@ -284,19 +284,49 @@ $(function(){
 
     // ============================================================================
     function onTouchStart(event){
+        preventPull(event);
+
         lastMove = event;
     }
 
     // ============================================================================
     function onTouchEnd(event){
+        preventPull(event);
+
         onMouseDown(lastMove.touches[0]);
     }
 
     // ============================================================================
     function onTouchMove(event){
-        lastMove = event;
+        preventPull(event);
 
-        console.log(lastMove.touches[0]);
-        onMouseMove(lastMove.touches[0]);
+        lastMove = event;
+    }
+
+    function preventPull() {
+      var previousTouches = {};
+      return function(event) {
+        var touches = Array.prototype.slice.call(event.touches);
+        nextTouches = {}
+        touches.forEach(function(touch){
+          nextTouches[touch.identifier] = touch;
+        });
+
+        if(document.scrollingElement.scrollTop > 0) {
+          previousTouches = nextTouches;
+          return;
+        }
+
+        for(var ix = 0; ix < touches.length; ix++) {
+          var touch = touches[ix],
+              id = touch.identifier;
+          if(id in previousTouches && previousTouches[id].screenY < touch.screenY) {
+            event.preventDefault();
+            console.log("event.preventDefault() called")
+            break;
+          }
+        }
+        previousTouches = nextTouches;
+      };
     }
 });
