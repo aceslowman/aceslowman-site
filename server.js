1fb06d980ae5cf05387619adb00539aead36d7a7
aceslowman <aceslowman@gmail.com>
2018-04-25 14:46:36 -0600

    Catching up

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.enable('strict routing');
 
-app.use(express.static('public', {redirect: false}));
+app.use(express.static('public'));
 
 app.use('/projects/weight_no_more',
   express.static('public/projects/weight_no_more/public'));
