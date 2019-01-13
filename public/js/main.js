0055d1ad757a21d10dfc055850adee42f421098e
aceslowman <aceslowman@gmail.com>
2019-01-13 14:18:03 -0700

    quick push to heroku

--- public/js/main.js
+++ public/js/main.js
@@ -12,7 +12,7 @@ let manager, debug, capturer, box, camera, capsule, light, text, post;
 
 const setup = () => {
   manager = new StandardManager();
-  manager.gui.close();
+  if(process.env.DEVELOPMENT) manager.gui.toggleHide();
   camera = new OrthographicCamera(manager);
   manager.setCamera(camera);
 
